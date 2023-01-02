b34e9a390506cd13be04e095ac3a1febbaced567
sravya307 <74953898+sravya307@users.noreply.github.com>
2023-01-02 10:15:36 +0530

    Add files via upload

---
+++ LinkedListTest.java
@@ -0,0 +1,24 @@
+package JavaExc;
+import java.util.*;
+public class LinkedListTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LinkedList<Integer> list = new LinkedList<>();
+		list.add(1);
+		list.add(2);
+		list.add(3);
+		list.add(4);
+		list.add(5);
+		
+		/*list.addFirst(100);
+		
+		list.addLast(500);
+		
+		list.add(1,200);*/
+		
+		System.out.println(list);
+
+	}
+
+}
